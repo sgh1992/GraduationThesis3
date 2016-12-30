abb6ec1b749331a3917c4e01574e9c0967b5df63
sgh1992 <1216688393@qq.com>
2016-12-30 21:26:20 +0800

    update in 1230 2130

--- src/main/java/consume/library/LibraryDoorWithSID.java
+++ src/main/java/consume/library/LibraryDoorWithSID.java
@@ -1,5 +1,7 @@
 package consume.library;
 
+import consume.tool.Tool;
+import consume.tool.YearAndTerm;
 import java.io.*;
 import java.util.HashMap;
 import java.util.HashSet;
@@ -9,40 +11,6 @@ import java.util.Set;
  * Created by sghipr on 2016/12/30.
  */
 public class LibraryDoorWithSID {
-
-    class SIDWithName {
-
-        private String studentID;
-
-        private String year;
-
-        public SIDWithName(String studentID, String year){
-
-            this.studentID = studentID;
-            this.year = year;
-
-        }
-
-        public int hashCode(){
-            return studentID.hashCode() + year.hashCode()*7;
-        }
-
-        public boolean equals(SIDWithName others){
-            return studentID.equals(others.studentID) && year.equals(others.year);
-        }
-
-        public String getStudentID(){
-            return studentID;
-        }
-
-        public String getName(){
-            return year;
-        }
-
-        public String toString(){
-            return studentID + "," + year;
-        }
-    }
 
     class StudentInfoRecord{
 
@@ -90,40 +58,76 @@ public class LibraryDoorWithSID {
 
     private String studentInfoFile = "D:\\GraduationThesis\\studentnameinfo.txt";
     private String libraryDoorFile = "D:\\GraduationThesis\\librarydoor2013_2014.txt";
-    private static String SIDFile = "D:\\GraduationThesis\\librarydoorWithStudentID.csv";
+    private static String LIBRARYDOORWITHID = "D:\\GraduationThesis\\librarydoorWithStudentID.csv";
 
-    public String getLibraryDoorWithStudentID(String studentInfoFile, String libraryDoorFile) throws IOException {
+    public LibraryDoorWithSID(){}
 
-        HashMap<String,Set<SIDWithName>> collegeAndIDMap = getIDInfo(studentInfoFile);
+    public File getLibraryDoorWithStudentID() throws IOException {
+        return getLibraryDoorWithStudentID(studentInfoFile,libraryDoorFile);
+    }
 
+    public File getLibraryDoorWithStudentID(String studentInfoFile, String libraryDoorFile) throws IOException {
+        HashMap<String,Set<String>> collegeAndIDMap = getIDInfo(studentInfoFile);
         LibraryDoorRecord parser = new LibraryDoorRecord();
-
-        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(libraryDoorFile)));
-
+        File root = new File(libraryDoorFile);
+        File dir = new File(root.getParent(),LIBRARYDOORWITHID);
+        BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(root)));
+        BufferedWriter writer = new BufferedWriter(new FileWriter(dir));
         String str = null;
         StringBuilder result = new StringBuilder();
         int wrongNums = 0;
-
+        YearAndTerm yearAndTerm = new YearAndTerm();
         while ((str = reader.readLine()) != null){
+            String studentID = null;
             if(parser.parser(str.trim())){
-                if(parser.getStudentID().length() != 0 && parser.getStudentID().startsWith("2010")){
-                    result.append(parser.getStudentID()).append(",").append(parser.getUserName()).append(",")
-                            .append(parser.getPassway()).append(",").append(parser.getPasstime()).append(",").append("2010");
+                String time = parser.getPasstime().replace("\\-","").substring(0,8);
+                if(parser.getStudentID().length() != 0){
+                    studentID = parser.getStudentID();
                 }
                 else if(parser.getRank().equals("本科生")){
                     String key = parser.getCollege() + "_" + parser.getUserName();
-
+                    studentID = getStudentID(key, time, collegeAndIDMap);
+                    }
+                if(studentID != null){
+                    if(Tool.getYearTerm(studentID,time,yearAndTerm)) {
+                        result.append(studentID).append(",").append(parser.getUserName()).append(",")
+                                .append(parser.getPassway()).append(",").append(parser.getPasstime()).append(",")
+                                .append(yearAndTerm.getYear()).append(",").append(yearAndTerm.getTerm());
+                        writer.write(result.toString());
+                        writer.newLine();
+                        result.setLength(0);
+                    }
                 }
             }
-
         }
+
+        reader.close();
+        writer.close();
+        return dir;
     }
 
-    public boolean
+    public String getStudentID(String key, String time, HashMap<String,Set<String>> map){
+        int nums = 0;
+        String SID = null;
+        if(map.containsKey(key)){
+            Set<String> studentIDSet = map.get(key);
+            for(String studentID : studentIDSet){
+                String year = studentID.startsWith("2010") ? "20100901" : "20090901";
+                if(time.compareTo(year) < 0)
+                    return null;
+                nums++;
+                SID = studentID;
+            }
+        }
+        if(nums > 1)
+            return null;
+        else
+            return SID;
+    }
 
-    public HashMap<String,Set<SIDWithName>> getIDInfo(String studentInfoFile) throws IOException {
+    public HashMap<String,Set<String>> getIDInfo(String studentInfoFile) throws IOException {
 
-        HashMap<String,Set<SIDWithName>> map = new HashMap<>();
+        HashMap<String,Set<String>> map = new HashMap<>();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(new FileInputStream(studentInfoFile)));
 
@@ -131,26 +135,30 @@ public class LibraryDoorWithSID {
 
         StudentInfoRecord parser = new StudentInfoRecord();
 
-        SIDWithName sidWithName = null;
-        Set<SIDWithName> set;
+        String studentID = null;
+        Set<String> set;
 
         while ((str = reader.readLine()) != null){
 
             if(parser.parser(str)){
                 String key = parser.getCollege() + "_" + parser.getUserName();
-                sidWithName = new SIDWithName(parser.getStudentID(),parser.getYear());
+                studentID = parser.getStudentID();
                 if(map.containsKey(key))
                     set = map.get(key);
                 else{
                     set = new HashSet<>();
-                    set.add(sidWithName);
+                    set.add(studentID);
                 }
                 map.put(key,set);
             }
-
         }
-
         reader.close();
         return map;
     }
+
+    public static void main(String[] args) throws IOException {
+        LibraryDoorWithSID libraryDoorWithSID = new LibraryDoorWithSID();
+        File result = libraryDoorWithSID.getLibraryDoorWithStudentID();
+    }
+
 }
